82b34584fd9cf94b8a9924abe4045ce3f9419b50
github user name <bje35149@gmail.com>
2021-05-08 14:15:44 +0900

    フォルダ名を変更
    axios-for-auth⇒axios-auth

--- src/axios-for-auth.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios'; //axiosの利用
-
-const instance = axios.create({
-  baseURL:
-  "https://firestore.googleapis.com/v1/projects/english-tube-c340d/databases/(default)/documents",
-  
-  });
-  
-  export default instance;
